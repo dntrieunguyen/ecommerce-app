998941d0c64817901f77d586401dac068cc22c5a
dntrieunguyen <dntrieunguyen@gmail.com>
2023-10-15 12:53:54 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.scss';
 
 function App() {
-   return <div className="App"></div>;
+   return (
+      <div>
+         <h2 className="font-bold text-transparent text-brand bg-gradient-to-tl from-primary to-error bg-clip-text">
+            This is ecommerce app
+         </h2>
+
+         <div className="w-[500px] h-[200px] bg-gradient-error"></div>
+      </div>
+   );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,31 @@
 
 module.exports = {
    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+   mode: 'jit',
    theme: {
-      extend: {},
+      extend: {
+         colors: {
+            light: '#F9FAFB',
+            primary: '#3B71CA',
+            secondary: '#9FA6B2',
+            success: '#14A44D',
+            warning: '#E4A11B',
+            error: '#DC4C64',
+            info: '#54B4D3',
+            dark: '#1F2937',
+         },
+
+         fontSize: {
+            content: '1.4rem',
+            lead: '2rem',
+            title: '2.4rem',
+            brand: '3rem',
+         },
+         backgroundColor: {
+            'gradient-error':
+               'linear-gradient(rgb(236, 72, 153), rgb(239, 68, 68), rgb(234, 179, 8))',
+         },
+      },
    },
    plugins: [],
 };
